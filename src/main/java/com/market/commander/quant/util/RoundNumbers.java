aea1d38cb97a96398ef5853edf1266a2ece9c938
Andrei <andrei.khaustov.nextlevel@gmail.com>
2025-04-06 16:05:52 +0300

    added stop session endpoint

--- src/main/java/com/market/commander/quant/util/RoundNumbers.java
+++ src/main/java/com/market/commander/quant/util/RoundNumbers.java
@@ -1,11 +1,15 @@
 package com.market.commander.quant.util;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
+@Slf4j
 public class RoundNumbers {
 
     public static Double toOpenPriceByAssetParam(Double tickSize, Double price) {
+        log.info("Tick size: {}; price: {}", tickSize, price);
         if (tickSize == null || tickSize <= 0 || price == null) {
             return null;
         }
@@ -16,6 +20,7 @@ public class RoundNumbers {
     }
 
     public static BigDecimal toAssetSize(Double marketLotSize, BigDecimal assetSize) {
+        log.info("Market lot size: {}; asset size: {}", marketLotSize, assetSize);
         if (marketLotSize == null || marketLotSize <= 0 || assetSize == null) {
             return assetSize.setScale(0, RoundingMode.HALF_UP);
         }
