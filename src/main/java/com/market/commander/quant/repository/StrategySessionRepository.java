53540f03f5985f8328d76859f746fb32b7d25dcb
Andrei <andrei.khaustov.nextlevel@gmail.com>
2025-04-06 12:07:48 +0300

    implementing quant service

--- src/main/java/com/market/commander/quant/repository/StrategySessionRepository.java
+++ src/main/java/com/market/commander/quant/repository/StrategySessionRepository.java
@@ -2,11 +2,13 @@ package com.market.commander.quant.repository;
 
 import com.market.commander.quant.entities.StrategySession;
 import com.market.commander.quant.enums.SessionStatus;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface StrategySessionRepository extends JpaRepository<StrategySession, Long> {
 
+    @EntityGraph(attributePaths = {"results", "user"})
     List<StrategySession> findByStatus(SessionStatus status);
 }
